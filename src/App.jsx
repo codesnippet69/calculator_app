90e62a58d02e3892376b90f9f44770bd14486c99
codesnippet69 <abuhurairajajja472@gmail.com>
2025-08-27 19:20:11 +0500

    Added provider and store in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
 
 import './App.css'
 import Buttons from './components/button'
+import { Provider } from 'react-redux'
+import store from './redux/store'
 
 function App() {
 
 
   return (
     <>
+    <Provider store={store}>
       <Buttons/>
+      </Provider>
     </>
   )
 }
