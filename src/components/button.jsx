a380c365642a000eb48f4aa5c2087ba1bcc9c4d8
codesnippet69 <abuhurairajajja472@gmail.com>
2025-08-27 19:18:19 +0500

    Added state on buttons and input fields from redux toolkit

--- src/components/button.jsx
+++ src/components/button.jsx
@@ -1,54 +1,49 @@
-
+import {useDispatch , useSelector} from 'react-redux'
+import { clearFields,backSpace,evaluate,appendValue } from '../redux/slices/slices';
 
 
 function Buttons(){
+    const expression=useSelector((state=>state.calculator.expression));
+    const result=useSelector((state=>state.calculator.result));
+    const dispatch=useDispatch();
+const buttons = [
+    "7","8","9","/","sin(","cos(","tan(","(",
+    "4","5","6","*","sqrt(","^",")",
+    "1","2","3","-","0",".","+",
+    "⌫","C","="
+  ];
+  const handleClick= (val)=>{
+    if (val =="C"){
+        dispatch(clearFields());
+    }
+         else if (val === "⌫") dispatch(backSpace());
+    else if (val === "=") dispatch(evaluate());
+    else dispatch(appendValue(val));
+    }
 
+
+  
     return(
 <>
 <input type="text"
+value={expression}
+readOnly
  />
 <br/>
 <input type="text"
-value={''}
+value={result}
+placeholder='Result'
 readOnly />
 
-<div>
-    <button label ="AC">AC</button>
-    <button label ="%">%</button>
-    <button label ="x">X</button>
-    <button label ="/">PI</button>
-    <button label ="^">^</button>
+<div className="Buttons" >
+     {buttons.map((btn) => (
+          <button key={btn} onClick={()=>handleClick(btn)} >
+            {btn}
+          </button>
+        ))}
 </div>
-<div>
-    <button label ="7">7</button>
-    <button label ="8">8</button>
-    <button label ="9">9</button>
-    <button label ="X">X</button>
-    <button label ="Squareroot">sqrt</button>
-</div>
-<div>
-    <button label ="4">4</button>
-    <button label ="5">5</button>
-    <button label ="6">6</button>
-    <button label ="+">+</button>
-    <button label ="sin">sin</button>
-</div>
-<div>
-    <button label ="1">1</button>
-    <button label ="2">2</button>
-    <button label ="3">3</button>
-    <button label ="-">-</button>
-    <button label ="cos">cos</button>
-</div>
-<div>
-    <button label ="0">0</button>
-    <button label =".">.</button>
-    <button label ="=">=</button>
-    <button label ="(">(</button>
-    <button label =")">)</button>
-    <button label ="tan">tan</button>
-    
-</div>
+
+
 </>
     )
 }
