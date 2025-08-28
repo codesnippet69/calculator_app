5349773e6bf0870e1a22483b835d683f02684ddc
codesnippet69 <abuhurairajajja472@gmail.com>
2025-08-28 18:02:17 +0500

    UI Dev and History Feature

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,23 @@
 import "./App.css";
 import Buttons from "./components/button";
+import History from "./components/history";
 import { Provider } from "react-redux";
 import store from "./redux/store";
 
 function App() {
   return (
-    <>
-      <Provider store={store}>
-        <Buttons />
-      </Provider>
-    </>
+    <Provider store={store}>
+      <div className="min-h-screen bg-neutral-900 text-neutral-100 flex items-start justify-center p-4">
+        <div className="w-full max-w-6xl flex flex-col md:flex-row gap-4">
+          <div className="flex-1 flex justify-center">
+            <Buttons />
+          </div>
+          <div className="w-full md:w-[320px] lg:w-[360px]">
+            <History />
+          </div>
+        </div>
+      </div>
+    </Provider>
   );
 }
 
